110d1a9cfe350a045c9c0c885602ebfef23b0191
Wangui-quinnie <w.wangui@ymail.com>
2025-05-06 12:06:09 +0300

    updated app.py to connect MySQL

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_mysqldb import MySQL
-from flask_cors import CORS
-from config import Config
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend interaction
-app.config.from_object(Config)
 
 mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    return "Task Manager API is running!"
+    return "Task Manager API is running and connected to MySQL!"
 
 if __name__ == '__main__':
     app.run(debug=True)
